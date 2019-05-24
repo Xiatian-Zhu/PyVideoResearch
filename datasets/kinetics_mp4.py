ca0d144da386a9a8f3ec7eae6c457c1c2e7a3320
xiatian.zhu <eddy.zhuxt@gmail.com>
2019-05-24 17:18:42 +0100

    get back to the original version

--- datasets/kinetics_mp4.py
+++ datasets/kinetics_mp4.py
@@ -20,8 +20,7 @@ class Kineticsmp4(Kinetics):
     def _get_video_path(self, path, vid, label):
         if not hasattr(self, 'int2cls'):
             self.int2cls = dict([(y, x) for x, y in self.cls2int.items()])
-        # name = self.int2cls[label['class']].replace(' ', '_')
-        name = self.int2cls[label['class']].replace(' ', '\ ')
+        name = self.int2cls[label['class']].replace(' ', '_')
         iddir = '{}/{}/{}_{:06d}_{:06d}.mp4'.format(
             path, name, vid, label['start'], label['end'])
         return iddir
