429b064dcf3192be3cd606d2eae50727a17e708b
xiatian.zhu <eddy.zhuxt@gmail.com>
2019-05-24 17:07:49 +0100

    rename

--- datasets/dataset_scripts/preprocess_kinetics.py
+++ datasets/dataset_scripts/preprocess_kinetics.py
@@ -6,7 +6,9 @@
 # Usage: ./download_kinetics_trim.py outdir/ csvfile.csv
 
 
-target_dir = '/home/SERILOCAL/x.chang/Datasets/Kinetics400/test_rename/'
+target_dir = '/home/SERILOCAL/x.chang/Datasets/Kinetics400/train/'
+# target_dir = '/home/SERILOCAL/xiatian.zhu/test_rename/'
+
 
 from os import listdir, rename
 from os.path import isfile, join, isdir
@@ -20,7 +22,7 @@ for f in listdir(target_dir):
         rename(join(target_dir, f), join(target_dir, fa))
 
         if isdir(join(target_dir, fa)) and not isfile(f):
-            print('Rename done: {0}'.format(fa))
+            print('Rename done: {0}\n'.format(fa))
 
 # onlyfiles = [f for f in listdir(target_dir) if isfile(join(mypath, f))]
 
