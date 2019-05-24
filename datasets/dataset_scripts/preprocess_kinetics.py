21161102e6d797ed63fad4461750e3da96c59616
xiatian.zhu <eddy.zhuxt@gmail.com>
2019-05-24 17:00:25 +0100

    add train script on cbb24: resnet50_3D for kinetics400

--- datasets/dataset_scripts/preprocess_kinetics.py
+++ datasets/dataset_scripts/preprocess_kinetics.py
@@ -6,17 +6,21 @@
 # Usage: ./download_kinetics_trim.py outdir/ csvfile.csv
 
 
-target_dir = '/home/SERILOCAL/x.chang/Datasets/Kinetics400/train/'
+target_dir = '/home/SERILOCAL/x.chang/Datasets/Kinetics400/train/test/'
 
-from os import listdir
+from os import listdir, rename
 from os.path import isfile, join, isdir
 
 for f in listdir(target_dir):
     if isdir(join(target_dir, f)):
         print('Before: {0}'.format(f))
-        f = f.replace(' ', '_')
-        print('After: {0}\n'.format(f))
+        fa = f.replace(' ', '_')
+        print('After: {0}'.format(fa))
 
+        rename(f, fa)
+
+        if isdir(join(target_dir, fa)) and not isfile(f):
+            print('Rename done: {0}'.format(fa))
 
 # onlyfiles = [f for f in listdir(target_dir) if isfile(join(mypath, f))]
 
