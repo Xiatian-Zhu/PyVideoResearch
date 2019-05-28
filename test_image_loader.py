b6d32091766787bd312da5d40302f0fed8ae5141
xiatian.zhu <eddy.zhuxt@gmail.com>
2019-05-28 17:48:14 +0100

    revise a test for image loader

--- test_image_loader.py
+++ test_image_loader.py
@@ -15,26 +15,27 @@ def read_one_iamge(path):
 
 def read_multi_images(target_dir):
     timer = Timer()
-
+    image_num = 0
     for f in listdir(target_dir):
         file_full_path = join(target_dir, f)
         if isfile(file_full_path):
             read_one_iamge(file_full_path)
+            image_num += 1
 
     time_cost = timer.thetime() - timer.end
     print('Load images from disk: {0:.3f} sec'.format(time_cost))
-    return time_cost
+    return time_cost,
 
 
 print('========== Image backend: {0}'.format(get_image_backend()))
 target_dir = '/home/nfs/x.chang/Datasets/Kinetics400/train_frms/lunge/Ku56XQb3N40_000004_000014/'
-time_cost_pil = read_multi_images(target_dir)
+time_cost_pil, image_num_pil = read_multi_images(target_dir)
 
 torchvision.set_image_backend('accimage')
 print('========== Image backend: {0}'.format(get_image_backend()))
-time_cost_acc = read_multi_images(target_dir)
+time_cost_acc, image_num_acc = read_multi_images(target_dir)
 
-print('Speedup by accimage: {time_cost_pil}/{time_cost_acc}')
+print('Speedup by accimage: {time_cost_pil}/{time_cost_acc} ({image_num_pil/image_num_acc})')
 
 
 
