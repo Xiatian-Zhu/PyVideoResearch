3ab8ec09bc660891c64595e166ee1bfb60b1526b
eddy <eddy.zhuxt@gmail.com>
2019-05-28 17:28:19 +0100

    test_image_loader.py

--- test_image_loader.py
+++ test_image_loader.py
@@ -1,5 +1,7 @@
 from datasets.utils import default_loader
+import torchvision
 
+torchvision.set_image_backend('accimage')
 
 path = '/home/nfs/x.chang/Datasets/Kinetics400/train_frms/lunge/Ku56XQb3N40_000004_000014/00000000.jpg'
 img = default_loader(path)
