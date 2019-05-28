c4d0aaee720f1e722a79c02692198f527eae3507
xiatian.zhu <eddy.zhuxt@gmail.com>
2019-05-28 10:52:19 +0100

    Change path for server

--- baseline_exp/_my_resnet50_3d_kinetics.py
+++ baseline_exp/_my_resnet50_3d_kinetics.py
@@ -3,12 +3,12 @@
 # using 4 GPUs
 # original name: i3d8k
 # model_best.txt:
-#     loss_train 1.3742251321368457
-#     loss_val 1.6312223851680756
-#     top1train 66.3049965893588
-#     top1val 64.01041666666667
-#     top5train 85.77336289222374
-#     top5val 83.69791666666667
+#     loss_train
+#     loss_val
+#     top1train
+#     top1val
+#     top5train
+#     top5val
 import sys
 import pdb
 import traceback
@@ -33,14 +33,14 @@ args = [
     '--train-size', '0.2',
     '--weight-decay', '0.0000001',
     '--val-size', '0.1',
-    '--cache-dir', '/home/SERILOCAL/xiatian.zhu/caches/Action_Recognition/',
+    '--cache-dir', '/home/nfs/xiatian.zhu/caches/Action_Recognition/',
     '--data', '/home/nfs/xiatian.zhu/datasets/Kinetics400/train/',
     '--valdata', '/home/nfs/xiatian.zhu/datasets/Kinetics400/val/',
     '--train-file', '/home/nfs/xiatian.zhu/datasets/Kinetics400/kinetics_train/kinetics_train.csv',
     '--val-file', '/home/nfs/xiatian.zhu/datasets/Kinetics400/kinetics_val/kinetics_val.csv',
     '--pretrained',
     '--nclass', '400',
-    '--resume', '/home/SERILOCAL/xiatian.zhu/caches/Action_Recognition/' + name + '/model.pth.tar',
+    '--resume', '/home/nfs/xiatian.zhu/caches/Action_Recognition/' + name + '/model.pth.tar',
     '--workers', '16',
     '--metric', 'val_top1',
     '--disable-cudnn-benchmark',
